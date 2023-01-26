7be5f2c410240cd20b2c69f622f7246406813f79
Tai Huynh <69029687+1BarCode@users.noreply.github.com>
2023-01-26 01:12:37 -0800

    feat: added react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import "@fontsource/roboto/700.css";
 import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -16,9 +17,15 @@ type AppPropsWithLayout = AppProps & {
     Component: NextPageWithLayout;
 };
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
     // Use the layout defined at the page level, if available
     const getLayout = Component.getLayout ?? ((page) => page);
 
-    return getLayout(<Component {...pageProps} />);
+    return (
+        <QueryClientProvider client={queryClient}>
+            {getLayout(<Component {...pageProps} />)}
+        </QueryClientProvider>
+    );
 }
